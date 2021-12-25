f0e54cf8fc845c750ee97fbbe86af26df9359280
[Ahmed El-Kilany] <[ahmedghjkhk@gmail.com]>
2021-12-25 19:38:37 +0200

    Done now testing for errors

--- PathPlanning.py
+++ PathPlanning.py
@@ -30,10 +30,51 @@ class AStarAlgorithm:
         plt.imshow(self.obstacle_map, origin='lower', cmap='gray')
         plt.scatter(x=[start.x, goal.x], y=[start.y, goal.y], c='b')
         plt.show()
+        open_nodes, closed_nodes = dict(),dict()
+        open_nodes[(start.o,start.x,start.y)] = start
+        
         while True:
+            if len(open_nodes) == 0:
+                print("No solution..")
+                break
 
+            current_i = min(open_nodes, key=lambda o: open_nodes[o].cost + self.heuristic(open_nodes[o],goal))
+            current = open_nodes[current_i]
+            
+            plt.scatter(current.x, current.y, color='r', alpha=0.3, marker='s')#, s=100)
+            
+            if current.x == goal.x and current.y == goal.y and current.o == goal.o:
+                print("Find goal")
+                goal.parent = current.parent
+                goal.cost = current.cost
+                break
+            # Remove the item from the open set
+            del open_nodes[current_i]
+
+            # Add it to the closed set
+            closed_nodes[current_i] = current
+
+            for i in np.where(self.motion[:,0] == current.o)[0]:
+                new_o = current.o + self.motion[i,1]
+                new_x = current.x + self.motion[i,2]
+                new_y = current.y + self.motion[i,3]
+                new_cost = current.cost + self.motion[i,4]
+                if new_x > self.max_x or new_x < self.min_x or new_y > self.max_y or new_y < self.min_y or obstacle_map[new_y, new_x] == -1:
+                    continue
+                open_nodes[(new_o,new_x,new_y)] = self.Node(new_o,new_x,new_y,current,new_cost)
+        return self.final_path(start,goal,closed_nodes)
+
+    def final_path(self,start,goal,closed_nodes):
+        y_list = []
+        x_list = []
+        current = goal
+        while current.parent != None:
+            y_list.append(current.y)
+            x_list.append(current.x)
+            current = current.parent
+        return x_list,y_list;
     @staticmethod
-    def heuristic(start, finish):
+    def heuristic(self,start, finish):
         return np.linalg.norm([start.get_coords(), finish.get_coords()])
 
     @staticmethod
@@ -44,47 +85,38 @@ class AStarAlgorithm:
         # 1: 90 degreee y_axis +ve
         # 2: 180 degree x_axis -ve
         # 3: 270 degree y_axis -ve
-        #           o   do      dx      dy      cost
-        motion = np.array([[0,   0,      10,     0,     10],     # 10 steps along the +ve x_axis
-                           # 10 steps along the +ve x_axis then a left turn and another 10 steps along +ve y_axis
-                           [0,   -1,     10,     10,    20],
-                           # 10 steps along the +ve x_axis then a right turn and another 10 steps along -ve y_axis
-                           [0,   1,      10,     -10,   20],
-                           # 10 steps along the +ve y_axis
-                           [1,   0,      0,      10,    10],
-                           # 10 steps along the +ve y_axis then a left turn and another 10 steps along -ve x_axis
-                           [1,   -1,     -10,    10,    20],
-                           # 10 steps along the +ve y_axis then a right turn and another 10 steps along +ve x_axis
-                           [1,   1,      10,     10,    20],
-                           # 10 steps along the -ve x_axis
-                           [2,   0,      -10,    0,     10],
-                           # 10 steps along the +ve x_axis then a left turn and another 10 steps along -ve y_axis
-                           [2,   -1,     -10,    -10,   20],
-                           # 10 steps along the +ve x_axis then a right turn and another 10 steps along +ve y_axis
-                           [2,   1,      -10,    10,    20],
-                           # 10 steps along the -ve y_axis
-                           [3,   0,      0,      -10,   10],
-                           # 10 steps along the -ve y_axis then a left turn and another 10 steps along +ve x_axis
-                           [3,   -1,     10,     -10,   20],
-                           [3,   1,      -10,    -10,   20]])    # 10 steps along the -ve y_axis then a right turn and another 10 steps along -ve x_axis
+        #                   o   do      dx      dy      cost
+        motion = np.array([[0,   0,      10,     0,     10],    # 10 steps along the +ve x_axis
+                           [0,   -1,     10,     10,    20],    # 10 steps along the +ve x_axis then a left turn and another 10 steps along +ve y_axis
+                           [0,   1,      10,     -10,   20],    # 10 steps along the +ve x_axis then a right turn and another 10 steps along -ve y_axis
+                           [1,   0,      0,      10,    10],    # 10 steps along the +ve y_axis
+                           [1,   -1,     -10,    10,    20],    # 10 steps along the +ve y_axis then a left turn and another 10 steps along -ve x_axis
+                           [1,   1,      10,     10,    20],    # 10 steps along the +ve y_axis then a right turn and another 10 steps along +ve x_axis
+                           [2,   0,      -10,    0,     10],    # 10 steps along the -ve x_axis
+                           [2,   -1,     -10,    -10,   20],    # 10 steps along the +ve x_axis then a left turn and another 10 steps along -ve y_axis
+                           [2,   1,      -10,    10,    20],    # 10 steps along the +ve x_axis then a right turn and another 10 steps along +ve y_axis
+                           [3,   0,      0,      -10,   10],    # 10 steps along the -ve y_axis
+                           [3,   -1,     10,     -10,   20],    # 10 steps along the -ve y_axis then a left turn and another 10 steps along +ve x_axis
+                           [3,   1,      -10,    -10,   20]])   # 10 steps along the -ve y_axis then a right turn and another 10 steps along -ve x_axis
 
         return motion
 
 
-# Defining the borders
-obstacle_map = np.zeros((1010, 1010))
-obstacle_map[:, :10] = -1
-obstacle_map[:10, :] = -1
-obstacle_map[:, -10:] = -1
-obstacle_map[-10:, :] = -1
+if __name__ == '__main__':
+    # Defining the borders
+    obstacle_map = np.zeros((1010, 1010))
+    obstacle_map[:, :10] = -1
+    obstacle_map[:10, :] = -1
+    obstacle_map[:, -10:] = -1
+    obstacle_map[-10:, :] = -1
 
-# Defining the obstacles
-obstacle_map[800:, :200] = -1
-obstacle_map[:800, 400:600] = -1
-obstacle_map[600:, 700:800] = -1
-plt.figure(figsize=(10, 10))
-plt.imshow(obstacle_map, origin='lower', cmap='gray')
-plt.scatter(x=[50, 950], y=[50, 950], c='b')
-plt.scatter(x=100, y=100, color='g', alpha=0.3, marker='s')
-plt.plot([1, 800], [1, 800])
-plt.show()
+    # Defining the obstacles
+    obstacle_map[800:, :200] = -1
+    obstacle_map[:800, 400:600] = -1
+    obstacle_map[600:, 700:800] = -1
+
+    
+    A_Star = AStarAlgorithm(obstacle_map=obstacle_map)
+    x, y = A_Star.search(50,50,0,950,950,1)
+
+
